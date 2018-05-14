a35446a1ec310b33642432434f515ab1b3af5829
ugesh <ugesh.roopchand@hotmail.com>
2018-05-14 15:22:23 +0400

    Creating server.component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+/*decorator */
+@Component( {
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
