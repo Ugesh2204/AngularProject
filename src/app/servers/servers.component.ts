82f6abe89c278986bedd9e33db13f07a0fd5214b
ugesh <ugesh.roopchand@hotmail.com>
2018-05-18 08:20:41 +0400

    Two way data biding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = '';
+  serverName = 'Testservers';
 
   constructor() { 
     //Ex6
@@ -26,7 +26,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus ='Server was created';
+    this.serverCreationStatus ='Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event){
